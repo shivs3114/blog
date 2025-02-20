d3be14b4a9ec73618b3a5f4c5ef52b636550f49d
shiva <shivangi31012004@gmail.com>
2025-02-20 23:06:09 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express=require('express');
+const expressLayout=require('express-ejs-layouts');
+
+const app=express();
+const PORT=5000 || process.env.PORT;
+
+app.use(express.static('public'));
+
+app.use(expressLayout);
+app.set('layout','./layout/main');
+app.set('view engine','ejs');
+
+app.use('/',require('./server/routes/main'));
+
+app.listen(PORT,()=>{console.log(`App listening on PORT ${PORT}`)});
