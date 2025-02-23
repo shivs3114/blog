8700547a8fe4c60aa133e81da6bd17a5b55bd7fd
shiva <shivangi31012004@gmail.com>
2025-02-24 01:33:30 +0530

    23/02/25

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,9 @@
 const express=require('express');
 const router=express.Router();
 
-router.get('',(req,res)=>{res.send("hola Shivangi");})
+router.get('',(req,res)=>{
+    const locals={title:"shivs"}
+    res.render('index',{locals});})
 
-
+router.get('/about',(req,res)=>{res.render('about');})
 module.exports=router;
